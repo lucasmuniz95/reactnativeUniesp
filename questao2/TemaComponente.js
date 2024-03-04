b6c2968a262746665137b0d06afea6af06aa8a51
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2024-03-04 14:25:18 -0300

    Update TemaComponente.js

--- questao2/TemaComponente.js
+++ questao2/TemaComponente.js
@@ -8,7 +8,6 @@ const ThemedComponent = () => {
     <div style={{ background: theme === 'light' ? '#ffffff' : '#333333', color: theme === 'light' ? '#333333' : '#ffffff', padding: '20px', textAlign: 'center' }}>
       <h2>Componente com Tema</h2>
       <p>O tema atual é: {theme}</p>
-      <button onClick={toggleTheme}>Alternar Tema</button>
     </div>
   );
 };
