b3ae0a447f02828ef3a6ec75fdfa290f21f70951
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2024-04-01 15:29:53 -0300

    Create App.js

---
+++ TED03/questao4/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return (
+    <div>
+      <Button title="Botão Vermelho" color="red" />
+      <Button title="Botão Verde" color="green" />
+      <Button title="Botão Azul" color="blue" />
+    </div>
+  );
+}
+
+export default App;
